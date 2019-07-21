e8558518d04889a1abfd4d10cb0fbf5dc58f404b
Redempt <instantiable@gmail.com>
2019-07-21 00:15:48 -0400

    Code cleanup, added gradle task for deployment, working on multi-block structure utility

--- src/redempt/redlib/RedLib.java
+++ src/redempt/redlib/RedLib.java
@@ -1,11 +1,11 @@
 package redempt.redlib;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class RedLib extends JavaPlugin implements Listener {
+public class RedLib extends JavaPlugin {
 	
 	public static String helpTitle;
 	public static String helpEntry;
@@ -14,7 +14,6 @@ public class RedLib extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		plugin = this;
-		getServer().getPluginManager().registerEvents(this, this);
 		FileConfiguration config = this.getConfig();
 		if (config.getString("helpTitle") == null) {
 			config.set("helpTitle", "&a--[ &eHelp for %cmdname% &a]--");
@@ -26,6 +25,12 @@ public class RedLib extends JavaPlugin implements Listener {
 		}
 		helpTitle = config.getString("helpTitle");
 		helpEntry = config.getString("helpEntry");
+	}
+	
+	public static String getServerVersion() {
+		String version = Bukkit.getVersion();
+		String[] split = version.split(" ");
+		return split[split.length - 1].trim().replace(")", "");
 	}
 	
 }
--- src/redempt/redlib/multiblock/MultiBlockStructure.java
+++ src/redempt/redlib/multiblock/MultiBlockStructure.java
@@ -1,7 +1,62 @@
 package redempt.redlib.multiblock;
 
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+import redempt.redlib.RedLib;
+
+/**
+ * A utility class to create interactive multi-block structures
+ * @author Redempt
+ *
+ */
 public class MultiBlockStructure {
 	
+	/**
+	 * Use this to get the info to build a multi-block structure.
+	 * Should be hard-coded.
+	 * @param start One bounding corner of the region
+	 * @param end The other bounding corner of the region
+	 * @return A string representing all of the block data for the region
+	 */
+	@SuppressWarnings("deprecation")
+	public static String stringify(Location start, Location end) {
+		if (!start.getWorld().equals(end.getWorld())) {
+			throw new IllegalArgumentException("Locations must be in the same  world");
+		}
+		int minX = Math.min(start.getBlockX(), end.getBlockX());
+		int minY = Math.min(start.getBlockY(), end.getBlockY());
+		int minZ = Math.min(start.getBlockZ(), end.getBlockZ());
+		
+		int maxX = Math.max(start.getBlockX(), end.getBlockX());
+		int maxY = Math.max(start.getBlockY(), end.getBlockY());
+		int maxZ = Math.max(start.getBlockZ(), end.getBlockZ());
+		
+		int midVersion = Integer.parseInt(RedLib.getServerVersion().split("\\.")[1]);
+		
+		String output = (maxX - minX) + "x" + (maxY - minY) + "x" + (maxZ - minZ) + ";";
+		for (int x = minX; x <= maxX; x++) {
+			for (int y = minY; y <= maxY; y++) {
+				for (int z = minZ; z <= maxZ; z++) {
+					Block block = new Location(start.getWorld(), x, y, z).getBlock();
+					block.getType();
+					if (midVersion >= 13) {
+						output += block.getBlockData().getAsString() + ";";
+					} else {
+						output += block.getType() + ":" + block.getData();
+					}
+				}
+			}
+		}
+		return output;
+	}
 	
+	public static enum Axis {
+		
+		X,
+		Y,
+		Z;
+		
+	}
 	
 }
