3a2b1fb87ecbb24adfe7241ba2f316b62bbfc2c0
Redempt <instantiable@gmail.com>
2020-02-13 20:26:06 -0500

    Added documentation for PlayerWrapper

--- src/redempt/redlib/misc/PlayerWrapper.java
+++ src/redempt/redlib/misc/PlayerWrapper.java
@@ -9,6 +9,13 @@ import org.bukkit.entity.Player;
 
 public class PlayerWrapper {
 	
+	/**
+	 * Wraps a player using a Proxy, disabling some methods from being called. Very hacky, do not use if it can be avoided.
+	 * Meant for when you want to send a fake event to test if it will be cancelled, but don't want the plugins to be able to do certain things with the player based on the event. 
+	 * @param player The player to wrap
+	 * @param disable The names of the methods to disable
+	 * @return The wrapped player
+	 */
 	public static Player wrap(Player player, String... disable) {
 		return (Player) Proxy.newProxyInstance(player.getClass().getClassLoader(), new Class<?>[] {Player.class}, new InvocationHandler() {
 			
