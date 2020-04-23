e220f3cf792a331b16297f131eae4e50760c1888
Redempt <instantiable@gmail.com>
2020-04-23 15:55:07 -0400

    Removed CommandBuilder, added Messages helper class

--- src/redempt/redlib/RedLib.java
+++ src/redempt/redlib/RedLib.java
@@ -1,13 +1,12 @@
 package redempt.redlib;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import redempt.redlib.commandmanager.CommandBuilder;
 import redempt.redlib.commandmanager.CommandFactory;
+import redempt.redlib.commandmanager.Messages;
 import redempt.redlib.dev.ItemHelper;
 import redempt.redlib.dev.StructureTool;
 
@@ -17,34 +16,22 @@ public class RedLib extends JavaPlugin {
 	public static Plugin plugin;
 	public static int midVersion = Integer.parseInt(getServerVersion().split("\\.")[1]);
 	
-	private void setDefault(FileConfiguration config, String key, String value) {
-		config.set(key, config.get(key, value));
-	}
-	
-	private void setConfigDefaults() {
-		FileConfiguration config = this.getConfig();
-		setDefault(config, "helpTitle", "&a--[ &eHelp for %cmdname% &a]--");
-		setDefault(config, "helpEntry", "&e%cmdname%&a: %help%");
-		setDefault(config, "noPermission", "&cYou do not have permission to run this command! (%permission%)");
-		setDefault(config, "firstLocationSet", "&aFirst location set!");
-		setDefault(config, "secondLocationSet", "&aSecond location set!");
-		setDefault(config, "cancelPromptMessage", "&cType '%canceltext%' to cancel.");
-		setDefault(config, "cancelText", "cancel");
-		setDefault(config, "mustHoldItem", "&cYou must be holding an item to do this!");
-		this.saveConfig();
-	}
+	private static Messages messages;
 	
 	public static String getMessage(String message) {
-		return ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString(message));
+		return messages.get(message);
 	}
 	
 	@Override
 	public void onEnable() {
 		plugin = this;
-		setConfigDefaults();
+		messages = Messages.load(this);
 		FileConfiguration config = this.getConfig();
 		if (config.contains("devMode")) {
 			devMode = config.getBoolean("devMode");
+		} else {
+			config.set("devMode", false);
+			
 		}
 		
 		if (devMode) {
@@ -53,15 +40,6 @@ public class RedLib extends JavaPlugin {
 					.register("redlib",
 					new ItemHelper(),
 					StructureTool.enable());
-		} else {
-			new CommandBuilder("struct", "structure", "itemhelper", "itemhelp", "ih")
-				.permission("redlib.admin")
-				.help("Developer mode is disabled.")
-				.hook((c) -> {
-					c.sendMessage(ChatColor.RED + "RedLib Developer Mode is disabled for this server.");
-					c.sendMessage(ChatColor.RED + "To enable Developer Mode, add 'devMode: true' to the RedLib config and restart the server.");
-				})
-				.build("redlib");
 		}
 	}
 	
---
+++ src/redempt/redlib/commandmanager/Messages.java
@@ -0,0 +1,133 @@
+package redempt.redlib.commandmanager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+
+/**
+ * Represents a list of messages loaded from a file with defaults
+ * @author Redempt
+ *
+ */
+public class Messages {
+	
+	/**
+	 * Loads messages from a file and writes missing defaults
+	 * @param plugin The plugin loading the messages
+	 * @param defaults The InputStream for default messages. Use {@link Plugin#getResource(String)} for this.
+	 * @param filename The name of the file in the plugin folder to load messages from
+	 * @return The Messages instance with messages loaded.
+	 */
+	public static Messages load(Plugin plugin, InputStream defaults, String filename) {
+		java.nio.file.Path path = plugin.getDataFolder().toPath().resolve(filename);
+		try {
+			Map<String, String> messages = Files.exists(path) ? parse(Files.lines(path)) : new HashMap<>();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(defaults));
+			Map<String, String> defaultMap = parse(Stream.generate(() -> {
+				try {
+					return reader.readLine();
+				} catch (IOException e) {
+					return null;
+				}
+			}));
+			boolean[] missing = {false};
+			defaultMap.forEach((k, v) -> {
+				if (!messages.containsKey(k)) {
+					messages.put(k, v);
+					missing[0] = true;
+				}
+			});
+			if (missing[0]) {
+				write(messages, path);
+			}
+			return new Messages(plugin, messages, defaultMap);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * Loads messages from a file, messages.txt, and writes missing defaults
+	 * @param plugin The plugin loading the messages
+	 * @param defaults The InputStream for default messages. Use {@link Plugin#getResource(String)} for this.
+	 * @return The Messages instance with messages loaded.
+	 */
+	public static Messages load(Plugin plugin, InputStream defaults) {
+		return load(plugin, defaults, "messages.txt");
+	}
+	
+	/**
+	 * Loads messages from a file, messages.txt, and writes missing defaults loaded from the plugin resource called messages.txt
+	 * @param plugin The plugin loading the messages
+	 * @return The Messages instance with messages loaded.
+	 */
+	public static Messages load(Plugin plugin) {
+		return load(plugin, plugin.getResource("messages.txt"), "messages.txt");
+	}
+	
+	private static Map<String, String> parse(Stream<String> input) {
+		Map<String, String> map = new HashMap<>();
+		input.allMatch(s -> {
+			if (s == null) {
+				return false;
+			}
+			int index = s.indexOf(':');
+			map.put(s.substring(0, index), s.substring(index + 1).trim());
+			return true;
+		});
+		return map;
+	}
+	
+	private static void write(Map<String, String> map, java.nio.file.Path file) {
+		List<String> lines = map.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue()).collect(Collectors.toList());
+		try {
+			Files.write(file, lines, Charset.defaultCharset(), StandardOpenOption.CREATE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private Plugin plugin;
+	private Map<String, String> messages;
+	private Map<String, String> defaults;
+	
+	private Messages(Plugin plugin, Map<String, String> messages, Map<String, String> defaults) {
+		this.messages = messages;
+		this.defaults = defaults;
+		this.plugin = plugin;
+	}
+	
+	/**
+	 * @return The plugin these messages belong to
+	 */
+	public Plugin getPlugin() {
+		return plugin;
+	}
+	
+	/**
+	 * Gets a color-formatted message by name
+	 * @param msg The name of the message
+	 * @return The message, which has been formatted with & as the color character.
+	 */
+	public String get(String msg) {
+		String message = messages.getOrDefault(msg, defaults.get(msg));
+		if (message == null) {
+			throw new IllegalArgumentException("Message '" + msg + "' does not have an assigned or default value!");
+		}
+		return ChatColor.translateAlternateColorCodes('&', message);
+	}
+	
+}
