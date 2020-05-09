cb8edd5f684ad03e52d7ee37ce203a07af7c070f
Redempt <instantiable@gmail.com>
2020-05-09 17:53:44 -0400

    Added WeightedRandom.java

--- src/redempt/redlib/misc/SQLHelper.java
+++ src/redempt/redlib/misc/SQLHelper.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * @author Redempt
+ */
 public class SQLHelper {
 	
 	/**
---
+++ src/redempt/redlib/misc/WeightedRandom.java
@@ -0,0 +1,55 @@
+package redempt.redlib.misc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Uses a map of outcomes to weights to get random values
+ * @param <T> The type of the value
+ * @author Redempt
+ */
+public class WeightedRandom<T> {
+	
+	private Map<T, Integer> weights;
+	private int total;
+	private List<T> list;
+	
+	/**
+	 * Creates a WeightedRandom using the map of weights
+	 * @param weights The map of outcomes to weights
+	 */
+	public WeightedRandom(Map<T, Integer> weights) {
+		this.weights = weights;
+		weights.forEach((k, v) -> {
+			total += v;
+			list.add(k);
+		});
+	}
+	
+	/**
+	 * Rolls and gets a weighted random outcome
+	 * @return A weighted random outcome
+	 */
+	public T roll() {
+		int random = (int) Math.round(Math.random() * total);
+		int pos = 0;
+		int roll = 0;
+		while (random > (roll = weights.get(list.get(pos)))) {
+			random -= roll;
+			pos++;
+		}
+		return list.get(pos);
+	}
+	
+	/**
+	 * Performs a single roll given a map of outcomes to weights. If you need to roll multiple times, instantiate a WeightedRandom and call roll on that each time instead.
+	 * @param map The map of outcomes to weights
+	 * @param <T> The type being returned
+	 * @return A weighted random outcome
+	 */
+	public static <T> T roll(Map<T, Integer> map) {
+		return new WeightedRandom<T>(map).roll();
+	}
+	
+}
