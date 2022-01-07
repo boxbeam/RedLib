44ea7d04fb22a70ce55daca9f3f6fa8587ef92d6
Redempt <instantiable@gmail.com>
2022-01-06 19:34:02 -0500

    Make UserCache initialize automatically on getOfflinePlayer call

--- src/redempt/redlib/misc/UserCache.java
+++ src/redempt/redlib/misc/UserCache.java
@@ -9,7 +9,9 @@ import redempt.redlib.RedLib;
 
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 
 /**
  * A cache of offline users by name which can be queried without worrying about web requests
@@ -18,15 +20,29 @@ import java.util.concurrent.ConcurrentHashMap;
 public class UserCache {
 	
 	private static Map<String, OfflinePlayer> nameCache;
+	private static CompletableFuture<Void> initTask;
 	
 	/**
 	 * Initializes the user cache asynchronously
 	 * @param onComplete A Runnable to be run when the initialization is complete
 	 */
-	public static void asyncInit(Runnable onComplete) {
-		Task.asyncDelayed(() -> {
-			init();
+	public static synchronized void asyncInit(Runnable onComplete) {
+		if (initTask != null || nameCache != null) {
 			onComplete.run();
+			return;
+		}
+		initTask = CompletableFuture.runAsync(() -> {
+			if (nameCache != null) {
+				return;
+			}
+			nameCache = new ConcurrentHashMap<>();
+			for (OfflinePlayer player : Bukkit.getOfflinePlayers()) {
+				nameCache.put(player.getName().toLowerCase(Locale.ROOT), player);
+			}
+			Task.syncDelayed(() -> new EventListener<>(RedLib.getInstance(), PlayerJoinEvent.class, EventPriority.LOWEST, e -> {
+				Player player = e.getPlayer();
+				nameCache.put(player.getName().toLowerCase(Locale.ROOT), player);
+			}));
 		});
 	}
 	
@@ -41,25 +57,25 @@ public class UserCache {
 	 * Initializes the user cache synchronously
 	 */
 	public static void init() {
-		if (nameCache != null) {
-			return;
+		asyncInit();
+		if (!initTask.isDone()) {
+			try {
+				initTask.get();
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
 		}
-		nameCache = new ConcurrentHashMap<>();
-		for (OfflinePlayer player : Bukkit.getOfflinePlayers()) {
-			nameCache.put(player.getName().toLowerCase(Locale.ROOT), player);
-		}
-		new EventListener<>(RedLib.getInstance(), PlayerJoinEvent.class, EventPriority.LOWEST, e -> {
-			Player player = e.getPlayer();
-			nameCache.put(player.getName().toLowerCase(Locale.ROOT), player);
-		});
 	}
 	
 	/**
-	 * Gets an OfflinePlayer by name
+	 * Gets an OfflinePlayer by name, initializing the cache synchronously if it has not been initialized yet
 	 * @param name The name of the player, case insensitive
 	 * @return The OfflinePlayer, or null
 	 */
 	public static OfflinePlayer getOfflinePlayer(String name) {
+		if (nameCache == null) {
+			init();
+		}
 		return nameCache.get(name.toLowerCase(Locale.ROOT));
 	}
 	
