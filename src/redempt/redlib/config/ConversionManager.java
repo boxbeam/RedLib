fcf7e57571903e4b39f35f4255362e3889dd3c3a
Redempt <instantiable@gmail.com>
2022-01-27 00:00:45 -0500

    Add primitive wrapper types for ConfigManager as string-converted

--- src/redempt/redlib/config/ConversionManager.java
+++ src/redempt/redlib/config/ConversionManager.java
@@ -35,6 +35,11 @@ public class ConversionManager {
 		convertersByType.put(new ConfigType<>(float.class), PrimitiveConverter.create(Float::parseFloat, String::valueOf));
 		convertersByType.put(new ConfigType<>(boolean.class), PrimitiveConverter.create(Boolean::parseBoolean, String::valueOf));
 		convertersByType.put(new ConfigType<>(long.class), PrimitiveConverter.create(Long::parseLong, String::valueOf));
+		convertersByType.put(new ConfigType<>(Integer.class), PrimitiveConverter.create(Integer::parseInt, String::valueOf));
+		convertersByType.put(new ConfigType<>(Double.class), PrimitiveConverter.create(Double::parseDouble, String::valueOf));
+		convertersByType.put(new ConfigType<>(Float.class), PrimitiveConverter.create(Float::parseFloat, String::valueOf));
+		convertersByType.put(new ConfigType<>(Boolean.class), PrimitiveConverter.create(Boolean::parseBoolean, String::valueOf));
+		convertersByType.put(new ConfigType<>(Long.class), PrimitiveConverter.create(Long::parseLong, String::valueOf));
 	}
 	
 	public ConversionManager(Plugin plugin) {
