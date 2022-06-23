8f8cfa5b0bf73c2a7531c49399d6fe616e38f5d6
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-06-23 17:57:30 +0200

    removed cloning of list 😅

--- src/redempt/redlib/itemutils/ItemUtils.java
+++ src/redempt/redlib/itemutils/ItemUtils.java
@@ -147,7 +147,7 @@ public class ItemUtils {
 	 */
 	public static ItemStack removeLoreLine(ItemStack item, String line){
 		ItemMeta meta = item.getItemMeta();
-		List<String> lore = new ArrayList<>(meta.getLore());
+		List<String> lore = meta.getLore();
 		lore.remove(line);
 		meta.setLore(lore);
 		item.setItemMeta(meta);
@@ -162,7 +162,7 @@ public class ItemUtils {
 	 */
 	public static ItemStack removeLoreLine(ItemStack item, int index){
 		ItemMeta meta = item.getItemMeta();
-		List<String> lore = new ArrayList<>(meta.getLore());
+		List<String> lore = meta.getLore();
 		if (index < 0 || index > lore.size()) {
                     throw new IllegalArgumentException("Value out of bounds (" + n + ")");
                 }
